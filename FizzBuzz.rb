452bd6ac6b03dddb1ed3b0b35138a48f74612717
Aaron Schreck <schreck.aaron@gmail.com>
2017-08-14 18:43:04 -0600

    initialize repository

---
+++ FizzBuzz.rb
@@ -0,0 +1,27 @@
+#if it’s evenly divisible by 3, 5, and 7 print SuperFizzBuzz
+#If it’s evenly divisible by 3 and 7 print SuperFizz
+#If it’s evenly divisible by 5 and 7 print SuperBuzz
+#If it’s evenly divisible by 3 and 5 print FizzBuzz
+#If it’s evenly divisible by 3, print Fizz
+#If it’s evenly divisible by 5, print Buzz
+#If it’s evenly divisible by 7, print Super
+#Otherwise print just the number
+
+def fizzbuzzer (number)
+
+word = ""
+	if number % 7 == 0 
+		word = "Super"
+	end
+
+ 	if number % 3 == 0
+		word = word + "Fizz"
+	end
+
+	if number % 5 == 0
+		word = word + "Buzz"
+	end
+
+puts word
+
+end
